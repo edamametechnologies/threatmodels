521bcb399e99e8c9113876d9a14d4d9ed0869fcf
Andrew Mary Huet de Barochez <amhdb@edamame.tech>
2023-06-07 14:21:59 +0200

    Modify: refactored python tests
    Used python logging package instead of prints, and implemented rollback tests

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from model import Model
+import logging
 
-model = Model(dir_path=".")
-model.run_implementations()
-model.run_remediations(force=True)
+logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', level=logging.INFO)
+
+logger = logging.getLogger('logger')
+
+model = Model(logger, dir_path=".")
+model.run_metrics_sequentially()
 results = model.get_results()
 
 print(results)
--- src/metric.py
+++ src/metric.py
@@ -1,108 +1,217 @@
 import subprocess
-from colorama import Fore, init
-from custom_exceptions import (
-        ImplementationTargetError, RemediationTargetError,
-        RemediationFixInvalid
-)
-
-# Initialize colorama
-init(autoreset=True)
 
 
 class Metric(object):
     '''Representation of a metric with associated functions to perform tests'''
 
-    def __init__(self, info, source):
+    def __init__(self, info, source, logger):
         self.info = info
         self.source = source
+        self.logger = logger
 
         # None: not run yet, False: no remediation, True: need remediation
         self.need_remediation = None
 
+        self.report = {
+                "implementation": {},
+                "remediation": {},
+                "rollback": {}
+        }
+
+    @staticmethod
+    def check_error_standard(result):
+        '''
+        Return a boolean indicating if the provided result was an error.
+        In this project we consider a command result as an error when the
+        returncode is not 0 and the stderr is not empty.
+
+        Parameter:
+            result: subprocess result
+        Return:
+            the error status of the result
+        '''
+        return result.returncode != 0 and result.stderr != ""
+
+    def metric_log(self, log_type, target_type, message, result=None):
+        if log_type == "error":
+            self.logger.error(f"[{target_type}] {message}")
+
+            if result is not None:
+                self.logger.error(result)
+
+    def is_target_cli(self, target_type):
+        '''Check if the given target type has a cli target'''
+        if self.info[target_type]['class'] != 'cli':
+            self.logger.info(f"target {target_type} is not a CLI")
+            return False
+        else:
+            return True
+
     def implementation(self):
         '''
         Execute the implementation and returns a boolean indicating if it needs
-        remediation
+        remediation. Return None if an error occured.
         '''
+        target_type = "implementation"
+        if not self.is_target_cli(target_type):
+            return None
+
         # Execute the target
-        result = self.exec(self.info["implementation"]["target"])
+        result = self.exec(target_type)
 
         # stdout with data means that a remediation is needed
         self.need_remediation = result.stdout != ""
 
-        if result.returncode != 0:
-            self.display_target(Fore.RED, "implementation", "ER")
-            self.print_result(result)
-            raise ImplementationTargetError
+        if self.check_error_standard(result):
+            self.metric_log(
+                    "error", target_type, "target returned an error", result)
+            return None
 
-        elif self.need_remediation:
-            self.display_target(Fore.YELLOW, "implementation", "NR")
-            self.print_result(result)
-        else:
-            self.display_target(
-                    Fore.GREEN, "implementation", "OK", show_target=False)
+        self.report[target_type]["result"] = result
 
         return self.need_remediation
 
     def remediation(self, force=False):
         '''
-        Execute the remediation and verify if the implementation has been fixed
+        Execute the remediation and return a boolen indicating if the
+        implementation has been fixed. Return None if an error occured.
         '''
+        target_type = "remediation"
+        if not self.is_target_cli(target_type):
+            return None
+
         if self.need_remediation or force:
             # Execute the target
-            result = self.exec(self.info["remediation"]["target"])
+            result = self.exec(target_type)
 
-            if result.returncode != 0:
-                self.display_target(Fore.RED, "remediation", "ER")
-                self.print_result(result)
-                raise RemediationTargetError
+            need_remediation = self.implementation()
 
-            # Verify implementation
-            elif self.implementation():
-                self.display_target(Fore.RED, "remediation", "IR")
-                self.print_result(result)
-                raise RemediationFixInvalid
+            if self.check_error_standard(result):
+                self.metric_log("error", target_type,
+                                "target returned an error", result)
+                return None
+            elif need_remediation is None:
+                return False
+            else:
+                return not need_remediation
 
     def rollback(self):
         '''
         Execute the rollback
         '''
-        self.exec(self.info["rollback"]["target"])
+        target_type = "rollback"
+        if not self.is_target_cli(target_type):
+            return None
 
-    def exec(self, target):
+        for i in range(0, 10):
+            result = self.exec(target_type)
+
+            if self.check_error_standard(result):
+                self.metric_log("error", target_type,
+                                "target returned an error", result)
+                return None
+
+            if not self.implementation():
+                self.metric_log("error", target_type,
+                                "rollback did not revert the change", result)
+                return None
+                # Not normal!!!
+                break
+
+            self.remediation()
+
+    def check_elevations(self, target_type):
+        '''
+        Checks if the elevations of each targets are not to high or too low.
+        Must run targets functions before
+        '''
+        if "need-perms" in self.report[target_type].keys():
+            need_perms = self.report[target_type]["need-perms"]
+
+            if need_perms is not None:
+                if need_perms:
+                    if self.info[target_type]["elevation"] == "user":
+                        # TODO: Optionally modify the permissions automatically
+
+                        self.metric_log("error", target_type,
+                                        "target permissions too low")
+                else:
+                    if self.info[target_type]["elevation"] != "user":
+                        # TODO: Optionally modify the permissions automatically
+
+                        self.metric_log("error", target_type,
+                                        "target permissions too high")
+
+    def exec(self, target_type):
         '''
         Execute a target on the corresponding platform and returns the result
+
+        Parameters:
+            target_type: implementation / remediation / rollback
+        Returns:
+            subprocess result
         '''
+        # Gets the corresponding target command
+        command = self.info[target_type]["target"]
+
         if self.source == "Windows":
-            result = subprocess.run(
-                    ["powershell.exe", "-Command", target],
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        elif self.source == "Linux" or self.source == "MacOS":
-            result = subprocess.run(
-                    target,
-                    shell=True,
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            command = f"powershell.exe -Command {command}"
+
+        result = subprocess.run(
+                command,
+                shell=True,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+
+        # If there is an error, try to run as root
+        if self.check_error_standard(result):
+            result = self.exec_as_root(target_type, command)
+        else:
+            self.report[target_type]["need-perms"] = False
+            self.report[target_type]["causes-error"] = False
+            self.report[target_type]["result"] = result
+
+        self.check_elevations(target_type)
 
         return result
 
-    def display_target(self, color, target_type,
-                       result_code, show_target=True):
+    def exec_as_root(self, target_type, command):
         '''
-        Display log informations for a specific target
+        Execute a target on the corresponding platform as root and returns the
+        result.
 
         Parameters:
-            color: colorama Fore color
-            target_type: implementation, remediation, rollback
-            result_code: OK (OK), ER (ERROR), IR (Invalid Remediation),
-                         NR (Need remediation)
-            show_target: show the target command
+            target_type: implementation / remediation / rollback
+            command: the command that couldn't be run without root privileges
+        Returns:
+            subprocess result
         '''
-        print(f"{color}[{result_code}][{target_type}] {self.info['name']}")
-        if show_target:
-            print(f"Target: {self.info[target_type]['target']}")
+        if self.source == "Windows":
+            command = f"elevate -w {command}"
+        else:
+            command = f"sudo -- sh -c '{command}'"
 
-    @staticmethod
-    def print_result(result):
-        print(f"Return code: {result.returncode}")
-        print(f"{Fore.GREEN}Stdout:\n{result.stdout}")
-        print(f"{Fore.RED}Stderr:\n{result.stderr}")
+        result = subprocess.run(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+
+        if self.check_error_standard(result):
+            # Error probably not related to permissions
+            self.report[target_type]["need-perms"] = None
+            self.report[target_type]["causes-error"] = True
+        else:
+            # Target need permissions
+            self.report[target_type]["need-perms"] = True
+            self.report[target_type]["causes-error"] = False
+
+        self.report[target_type]["result"] = result
+
+        return result
+
+    def run_all_tests(self):
+        self.logger.info(f"Running `{self.info['name']} tests`")
+        self.implementation()
+        self.remediation()
+        self.rollback()
