a4172ed4b38fdb0de9f25fed0558ce94cd8f58e8
Andrew Mary Huet de Barochez <amhdb@edamame.tech>
2023-06-09 15:00:30 +0200

    Add: write report into a txt file for github action outputs

--- src/model.py
+++ src/model.py
@@ -51,4 +51,7 @@ class Model(object):
             for key in res.keys():
                 res[key] += report[key]
 
+        with open('report-results.txt', 'w') as file:
+            file.write(res)
+
         return res
