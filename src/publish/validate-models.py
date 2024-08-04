128bfc9da11c5c807d0d32403eeee642bf479964
Frank Lyonnet <flyonnet@edamame.tech>
2024-08-04 10:04:49 +0200

    FIX: models, validation

--- src/publish/validate-models.py
+++ src/publish/validate-models.py
@@ -285,6 +285,17 @@ def validate_threat_model(filename: str) -> None:
                             raise ValueError(f"Invalid URL '{impl['target']}' in target field at '{metric['name']} -> {key} -> impl[{k}]'. "
                                              f"Reason: {response.status_code} {response.reason}")
 
+            # If we have a youtube class, check the URL is a valid youtube video
+            if impl['class'] == 'youtube':
+                if not impl['target'].startswith("https://www.youtube.com/watch?v="):
+                    raise ValueError(f"Invalid URL '{impl['target']}' in target field at '{metric['name']} -> {key} -> impl[{k}]'. "
+                                     f"URL must start with 'https://www.youtube.com/watch?v='")
+
+            # If we have a youtube URL, check if the class is 'youtube'
+            if impl['target'].startswith("https://www.youtube.com/watch?v=") and impl['class'] != 'youtube':
+                raise ValueError(f"Invalid class '{impl['class']}' in class field at '{metric['name']} -> {key} -> impl[{k}]'. "
+                                 f"Class must be 'youtube'")
+
             # Validate 'education' in 'implementation'
             for k, education in enumerate(impl['education']):
                 if set(education.keys()) != allowed_keys_education:
@@ -294,8 +305,8 @@ def validate_threat_model(filename: str) -> None:
                 if not all(isinstance(education[field], str) for field in allowed_keys_education):
                     raise ValueError(f"Invalid data type in education fields at '{metric['name']} -> {key} -> education[{k}]'")
 
-                # If we have a class "link" or "youtube" or "installer" check the url is valid and try to access it to check if we have a 404
-                if education['class'] in ['link', 'youtube', 'installer']:
+                # If we have a class "link" or "youtube" check the url is valid and try to access it to check if we have a 404
+                if education['class'] in ['link', 'youtube']:
                     # Only check http(s) links
                     if education['target'].startswith("http"):
                         response = requests.head(education['target'])
@@ -305,6 +316,17 @@ def validate_threat_model(filename: str) -> None:
                             if not response.ok:
                                 raise ValueError(f"Invalid URL '{education['target']}' in target field at '{metric['name']} -> {key} -> education[{k}]'. "
                                              f"Reason: {response.status_code} {response.reason}")
+
+                    # If we have a youtube class, check the URL is a valid youtube video
+                    if education['class'] == 'youtube':
+                        if not education['target'].startswith("https://www.youtube.com/watch?v="):
+                            raise ValueError(f"Invalid URL '{education['target']}' in target field at '{metric['name']} -> {key} -> impl[{k}]'. "
+                                             f"URL must start with 'https://www.youtube.com/watch?v='")
+
+                    # If we have a youtube URL, check if the class is 'youtube'
+                    if education['target'].startswith("https://www.youtube.com/watch?v=") and education['class'] != 'youtube':
+                        raise ValueError(f"Invalid class '{impl['class']}' in class field at '{metric['name']} -> {key} -> impl[{k}]'. "
+                                         f"Class must be 'youtube'")
 
             # Type checks for other fields in implementation/remediation/rollback
             if not isinstance(impl['system'], str) or \
